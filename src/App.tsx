3c5a3edd49926063f623f3c4e09ba0a77a3c5ec3
William Silva <euwilliamsantana@gmail.com>
2022-10-01 08:13:59 -0300

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,15 @@ export function App() {
  
   return (
    <C.Container>
-    ..
+    <C.Header>
+      <C.HeaderText>
+        Sistema Financeiro
+      </C.HeaderText>
+    </C.Header>
+
+    <C.Body>
+      ...
+    </C.Body>
 
    </C.Container>
   )
